ee1c92cca7f617c73c88cb42282c505d515d0d8e
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2021-06-16 16:01:48 -0700

    attempting to set up online db

--- src/components/create-job.component.js
+++ src/components/create-job.component.js
@@ -3,7 +3,7 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 
-var mongoURI = 'mongodb+srv://TrackerAdmin:TrackerAdminPassword@jobs.euzmb.mongodb.net/TrackerDatabase?retryWrites=true&w=majority';
+var mongoURI = 'mongodb+srv://TrackerAdmin:TrackerAdminPassword@cluster0.euzmb.mongodb.net/TrackerDatabase?retryWrites=true&w=majority';
 
 export default class CreateJob extends Component {
 
